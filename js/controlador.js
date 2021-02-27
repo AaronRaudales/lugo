01160982f05ba386597d31bde4566961d1d6df59
AaronRaudales <aaronraudales83@gmail.com>
2021-02-27 12:00:14 -0600

    Vamos viendo

---
+++ js/controlador.js
@@ -0,0 +1,567 @@
+///-----------------------------------------------USUARIOS
+var usuarios = [
+    {
+        nombre: "Pedro",
+        apellido: "Martinez",
+        ordenes: [
+            {
+                nombreProducto: "Producto 1",
+                descripcion: "Lorem ipsum, dolor sit amen",
+                cantidad: 2,
+                precio: 49.99
+            },
+            {
+                nombreProducto: "Producto 2",
+                descripcion: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore, modi!",
+                cantidad: 2,
+                precio: 49.99
+            }
+        ]
+    },
+    {
+        nombre: "Juan",
+        apellido: "Perez",
+        ordenes: [
+            {
+                nombreProducto: "Producto 3",
+                descripcion: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore, modi!",
+                cantidad: 2,
+                precio: 49.99
+            },
+            {
+                nombreProducto: "Producto 2",
+                descripcion: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore, modi!",
+                cantidad: 2,
+                precio: 49.99
+            }
+        ]
+    },
+    {
+        nombre: "Maria",
+        apellido: "Rodriguez",
+        ordenes: [
+            {
+                nombreProducto: "Producto 4",
+                descripcion: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore, modi!",
+                cantidad: 2,
+                precio: 49.99
+            },
+            {
+                nombreProducto: "Producto 1",
+                descripcion: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore, modi!",
+                cantidad: 2,
+                precio: 49.99
+            }
+        ]
+    }
+];
+
+
+///-----------------------------------------------CATEGORIAS
+var categorias = [
+    {
+        nombreCategoria: "Farmacias",
+        descripcion: "3 Comercios",
+        color: "#FFD929",
+        icono: "fas fa-prescription-bottle-alt",
+        empresas: [
+            {
+                nombreEmpresa: "Empresa 1",
+                imagen: "img/banner.jpg",
+                productos: [
+                    {
+                        nombreProducto: "Producto 1",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    {
+                        nombreProducto: "Producto 2",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    {
+                        nombreProducto: "Producto 3",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    
+                ]
+            },
+            {
+                nombreEmpresa: "Empresa 2",
+                imagen: "img/banner.jpg",
+                productos: [
+                    {
+                        nombreProducto: "Producto 1",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    {
+                        nombreProducto: "Producto 2",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    {
+                        nombreProducto: "Producto 3",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    
+                ]
+            },
+            {
+                nombreEmpresa: "Empresa 3",
+                imagen: "img/banner.jpg",
+                productos: [
+                    {
+                        nombreProducto: "Producto 1",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    {
+                        nombreProducto: "Producto 2",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    {
+                        nombreProducto: "Producto 3",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    
+                ]
+            }
+        ]
+    },
+    {
+        nombreCategoria: "Restaurantes",
+        descripcion: "3 Comercios",
+        color: "#FFCDC5",
+        icono: "fas fa-utensils",
+        empresas: [
+            {
+                nombreEmpresa: "Empresa 1",
+                imagen: "img/banner.jpg",
+                productos: [
+                    {
+                        nombreProducto: "Producto 1",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    {
+                        nombreProducto: "Producto 2",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    {
+                        nombreProducto: "Producto 3",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    
+                ]
+            },
+            {
+                nombreEmpresa: "Empresa 2",
+                imagen: "img/banner.jpg",
+                productos: [
+                    {
+                        nombreProducto: "Producto 1",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    {
+                        nombreProducto: "Producto 2",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    {
+                        nombreProducto: "Producto 3",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    
+                ]
+            },
+            {
+                nombreEmpresa: "Empresa 3",
+                imagen: "img/banner.jpg",
+                productos: [
+                    {
+                        nombreProducto: "Producto 1",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    {
+                        nombreProducto: "Producto 2",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    {
+                        nombreProducto: "Producto 3",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    
+                ]
+            }
+        ]
+    },
+    {
+        nombreCategoria: "Salud",
+        descripcion: "3 Comercios",
+        color: "#DB8462",
+        icono: "fas fa-hospital",
+        empresas: [
+            {
+                nombreEmpresa: "Empresa 1",
+                imagen: "img/banner.jpg",
+                productos: [
+                    {
+                        nombreProducto: "Producto 1",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    {
+                        nombreProducto: "Producto 2",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    {
+                        nombreProducto: "Producto 3",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    
+                ]
+            },
+            {
+                nombreEmpresa: "Empresa 2",
+                imagen: "img/banner.jpg",
+                productos: [
+                    {
+                        nombreProducto: "Producto 1",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    {
+                        nombreProducto: "Producto 2",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    {
+                        nombreProducto: "Producto 3",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    
+                ]
+            },
+            {
+                nombreEmpresa: "Empresa 3",
+                imagen: "img/banner.jpg",
+                productos: [
+                    {
+                        nombreProducto: "Producto 1",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    {
+                        nombreProducto: "Producto 2",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    {
+                        nombreProducto: "Producto 3",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    
+                ]
+            }
+        ]
+    },
+    {
+        nombreCategoria: "Café",
+        descripcion: "3 Comercios",
+        color: "#9781C9",
+        icono: "fas fa-coffee",
+        empresas: [
+            {
+                nombreEmpresa: "Empresa 1",
+                imagen: "img/banner.jpg",
+                productos: [
+                    {
+                        nombreProducto: "Producto 1",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    {
+                        nombreProducto: "Producto 2",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    {
+                        nombreProducto: "Producto 3",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    
+                ]
+            },
+            {
+                nombreEmpresa: "Empresa 2",
+                imagen: "img/banner.jpg",
+                productos: [
+                    {
+                        nombreProducto: "Producto 1",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    {
+                        nombreProducto: "Producto 2",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    {
+                        nombreProducto: "Producto 3",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    
+                ]
+            },
+            {
+                nombreEmpresa: "Empresa 3",
+                imagen: "img/banner.jpg",
+                productos: [
+                    {
+                        nombreProducto: "Producto 1",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    {
+                        nombreProducto: "Producto 2",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    {
+                        nombreProducto: "Producto 3",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    
+                ]
+            }
+        ]
+    },
+    {
+        nombreCategoria: "Bebidas",
+        descripcion: "3 Comercios",
+        color: "#FFA87D",
+        icono: "fas fa-cocktail",
+        empresas: [
+            {
+                nombreEmpresa: "Empresa 1",
+                imagen: "img/banner.jpg",
+                productos: [
+                    {
+                        nombreProducto: "Producto 1",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    {
+                        nombreProducto: "Producto 2",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    {
+                        nombreProducto: "Producto 3",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    
+                ]
+            },
+            {
+                nombreEmpresa: "Empresa 2",
+                imagen: "img/banner.jpg",
+                productos: [
+                    {
+                        nombreProducto: "Producto 1",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    {
+                        nombreProducto: "Producto 2",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    {
+                        nombreProducto: "Producto 3",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    
+                ]
+            },
+            {
+                nombreEmpresa: "Empresa 3",
+                imagen: "img/banner.jpg",
+                productos: [
+                    {
+                        nombreProducto: "Producto 1",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    {
+                        nombreProducto: "Producto 2",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    {
+                        nombreProducto: "Producto 3",
+                        descripcion: "Lorem ipsum, dolor sit amen",
+                        precio: 49.99
+                    },
+                    
+                ]
+            }
+        ]
+    }
+];
+
+var localstorage = window.localStorage;
+var indiceAppSeleccionada = null;
+
+if(localstorage.getItem('usuarios') == null){
+    localstorage.setItem('usuarios', JSON.stringify(usuarios));
+} else {
+    usuarios = JSON.parse(localstorage.getItem('usuarios')); 
+}
+
+if(localstorage.getItem('categorias') == null){
+    localstorage.setItem('categorias', JSON.stringify(categorias));
+} else {
+    categorias = JSON.parse(localstorage.getItem('categorias')); 
+}
+
+function cambiarUsuario(){
+    console.log("Usuario: "+ document.getElementById("usuarioActual").value);
+}
+
+
+for (let i = 1; i <= 1; i++) {
+    usuarios.forEach(function (app1) {
+        document.getElementById('usuarioActual').innerHTML +=
+            `<option>${app1.nombre}</option>`
+    });
+}
+
+function generarCategorias() {
+    categorias.forEach(function (app, i) {
+        document.getElementById('categorias').innerHTML +=
+            `<di class="col-sm-12 col-md-6  col-lg-4 col-xl-3">
+            <div class="card" style="background-color:${app.color}" onclick="generarEmpresas()" id="cuadros">
+                <i class="${app.icono}" id="picture"></i>
+                <div class="card-body">
+                    <h5 class="card-title texto" >${app.nombreCategoria}</h5>
+                    <p class="card-text">${app.descripcion}</p>
+                </div>
+            </div>
+        </di>`;
+    });
+}
+
+generarCategorias();
+
+
+
+function generarEmpresas() {
+    $('#modalVerEmpresa').modal('show')
+    categorias.forEach(function (app2, i) {
+        document.getElementById('empresas').innerHTML = '';
+        app2.empresas.forEach(function(app3,i){
+            document.getElementById('empresas').innerHTML +=
+            `<div class="row">
+                <div class="col-6">
+                    <div class="card">
+                        <div class="contenedor">
+                            <img src="img/banner.jpg" class="card-img-top" alt="...">
+                            <div class="centrado">${app3.nombreEmpresa}</div>
+                         </div>
+                    </div>`
+                
+                    app3.productos.forEach(function(app4,i){
+                    document.getElementById('empresas').innerHTML +=
+                    `<div class="row">
+                        <div class="col-12">
+                            <div class="card-body" id="contorno">
+                                <p class="card-text texto-negrita">${app4.nombreProducto}</p>
+                                <p class="card-text">${app4.descripcion}</p>
+                                <p class="card-text">${app4.precio}</p>
+                                <button type="button" onclick="generarPedido(${i})" id="button1">Pedir</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+            });             
+            
+            
+        });
+        
+    });
+
+    $('#modalVerEmpresa').modal('hide')
+}
+
+
+
+function generarPedido(indice){
+    $('#modalNuevoPedido').modal('show')
+   categorias.forEach(function (app2, i) {
+    app2.empresas.forEach(function(app3,i){
+        app3.productos.forEach(function(app4,i){
+            document.getElementById('pedidos').innerHTML =
+            `<section class="contorno">
+                <p class="texto-negrita" id="color">${app4.nombreProducto}</p>
+                <p >${app4.descripcion}</p>
+                Cantidad a Solicitar: <input type="number" id="numero"><br><br>
+                <p style="float:right">$ ${app4.precio}</p>
+            </section>`;
+
+        }); 
+    });
+    });
+}
+
+
+function informacionPedido(){
+    document.getElementById('modalVerOrdenesLabel').innerHTML =
+    `<p id="Titulo1">${document.getElementById('usuarioActual').value} ${', Estas son tus ordenes'}</p>`;
+    usuarios.forEach(function(user){
+        if(document.getElementById('usuarioActual').value == user.nombre ){
+            document.getElementById('PedidoUsuario').innerHTML ='';
+           user.ordenes.forEach(function(orden){
+            document.getElementById('PedidoUsuario').innerHTML +=
+            `<section class="contorno">
+                <p class="texto-negrita" >${orden.nombreProducto}</p>
+                <p>${orden.descripcion}</p>
+                <p style="float:right">$ ${orden.precio}</p><br>
+                <div class="linea"></div>
+            </section>`;
+            
+            });
+        }
+    });
+
+}
+
+function cerrar(){
+    $('#modalNuevoPedido').modal('hide')
+}
+
+
+console.log('Usuarios', usuarios);
+console.log('Categorias', categorias);
